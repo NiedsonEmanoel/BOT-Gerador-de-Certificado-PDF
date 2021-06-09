034f58a1a2f895574096dd5d64bd8ec77a9a537b
Niedson Emanoel <niedsonemanoeltbm@gmail.com>
2021-06-09 03:26:38 -0300

    gerador pronto

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ const ejs = require('ejs');
 let html;
 let initialDate = new Date();
 
-ejs.renderFile('./base.ejs', {name: 'Niedson Emanoel Almeida Brito'}, (err, data) => {
+ejs.renderFile('./base.ejs', {
+    title: "CERTIFICADO",
+    subtitle: "Confiro o presente certificado a",
+    name: 'Niedson Emanoel Almeida Brito',
+    initialDescription: "Por assistir a palestra",
+    boldDescription: "Destruindo a Skynet: práticas para Machine Learning responsável com Bianca Ximenes",
+    finalDescription: "com total de 1 hora e 30 minutos. A palestra foi realizada no evento The Developer's Conference (thedevconf.com).",
+    entity: "Niedson Emanoel",
+    entityCredential: "Programador sem nada pra fazer de madruga."
+}, (err, data) => {
     if (err) {
         console.log(err)
     } else {
