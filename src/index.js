d067c9e5d743e649645eaafbab6f7707998ed850
Niedson Emanoel <niedsonemanoeltbm@gmail.com>
2021-06-09 03:34:44 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,44 +1,47 @@
-const pdf = require('html-pdf');
-const ejs = require('ejs');
-let html;
-let initialDate = new Date();
+const gerarCertificado = async (title, subtitle, name, initialDescription, boldDescription, finalDescription, entity, entityCredential) => {
+    const pdf = require('html-pdf');
+    const ejs = require('ejs');
+    let html;
 
-ejs.renderFile('./base.ejs', {
-    title: "CERTIFICADO",
-    subtitle: "Confiro o presente certificado a",
-    name: 'Niedson Emanoel Almeida Brito',
-    initialDescription: "Por assistir a palestra",
-    boldDescription: "Destruindo a Skynet: práticas para Machine Learning responsável com Bianca Ximenes",
-    finalDescription: "com total de 1 hora e 30 minutos. A palestra foi realizada no evento The Developer's Conference (thedevconf.com).",
-    entity: "Niedson Emanoel",
-    entityCredential: "Programador sem nada pra fazer de madruga."
-}, (err, data) => {
-    if (err) {
-        console.log(err)
-    } else {
-        html = data
-    }
-})
-
-
-pdf.create(html, {
-    format: 'A4',
-    orientation: 'landscape',
-}).toFile('./certificate.pdf', (err, res) => {
-    if (err) {
-        console.error(err)
-    } else {
-        console.log(res)
-
-        let timeFinal = (new Date().getTime() - initialDate.getTime()) / 1000
-
-        console.log('Tempo real: ', timeFinal.toFixed(2), 'Segundos.')
-
-        while (timeFinal % 2 !== 0) {
-            timeFinal = (new Date().getTime() - initialDate.getTime()) / 1000
+    ejs.renderFile('./base.ejs', {
+        title,
+        subtitle,
+        name,
+        initialDescription,
+        boldDescription,
+        finalDescription,
+        entity,
+        entityCredential
+    }, (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            html = data
         }
+    })
 
-        console.log('Tempo arredondado: ', timeFinal.toFixed(2), 'Segundos.')
-    }
-})
 
+    pdf.create(html, {
+        format: 'A4',
+        orientation: 'landscape',
+    }).toFile('./certificate.pdf', (err, res) => {
+        if (err) {
+            console.error(err)
+        } else {
+            return res.filename;
+        }
+    })
+}
+
+(async () => {
+    await gerarCertificado(
+        "CERTIFICADO",
+        "Confiro o presente certificado a",
+        'Niedson Emanoel Almeida Brito',
+        "Por assistir a palestra",
+        "Destruindo a Skynet: tetagshjkshskgsdsdg",
+        "com total de 1 hora e 30 minutos. A palestra foi realizada no evento kkkkkkkkkkkkkkkkkkkkkkkkkkkkkk.",
+        "Niedson Emanoel",
+        "Programador sem nada pra fazer de madruga."
+    )
+})();
