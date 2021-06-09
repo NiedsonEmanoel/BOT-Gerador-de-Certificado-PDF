69d43abb485d50dd5dbf661b017dc67a17341e92
Niedson Emanoel <niedsonemanoeltbm@gmail.com>
2021-06-09 02:08:19 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const pdf = require('html-pdf');
 const ejs = require('ejs');
 let html;
+let initialDate = new Date();
 
 ejs.renderFile('./base.ejs', {}, (err, data) => {
     if (err) {
@@ -14,11 +15,21 @@ ejs.renderFile('./base.ejs', {}, (err, data) => {
 pdf.create(html, {
     format: 'A4',
     orientation: 'landscape',
-
-}).toFile('./niedson.pdf', (err, res) => {
+}).toFile('./certificate.pdf', (err, res) => {
     if (err) {
         console.error(err)
     } else {
         console.log(res)
+
+        let timeFinal = (new Date().getTime() - initialDate.getTime()) / 1000
+
+        console.log('Tempo real: ', timeFinal.toFixed(2), 'Segundos.')
+
+        while (timeFinal % 2 !== 0) {
+            timeFinal = (new Date().getTime() - initialDate.getTime()) / 1000
+        }
+
+        console.log('Tempo arredondado: ', timeFinal.toFixed(2), 'Segundos.')
     }
 })
+
