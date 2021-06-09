9f3ca0cce15e3c09ebcdd2857510aee5a03a5afe
Niedson Emanoel <niedsonemanoeltbm@gmail.com>
2021-06-09 05:37:44 -0300

    geradora

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# BOT Gerador de Certificado PDF
- Bot que gera certificados. Somente autorizo o uso para eventos..
+# Geradora de Certficados PDF'S
+Biblioteca simples feita com o ejs com o instuito de gerar certificados em pdf.
+
+## Para instalar:
+```
+npm i lib-geradora-de-certificado-pdf
+```
+
+
+## O código abaixo gera o seguinte certificado:
+```
+
+```
+
+<img src=""/>
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-const gerarCertificado = (title, subtitle, name, initialDescription,
+module.exports = function (
+    title,
+    subtitle,
+    name,
+    initialDescription,
     boldDescription,
     finalDescription,
     entity,
-    entityCredential) => {
+    entityCredential,
+    path
+) {
     return new Promise(async (resolve, reject) => {
         const { promisify } = require('util')
         const pdf = require('html-pdf');
-        const uuid = require('uuid').v4;
         let html;
 
-        const renderFile = promisify(require('ejs').renderFile);
+        path.replace('.pdf', '')
+        path = `${path}.pdf`
 
-        await renderFile('./base.ejs', {
+        const renderFile = promisify(require('ejs').renderFile);
+        console.log('Gerando certificado...')
+        await renderFile(__dirname + '/base.ejs', {
             title,
             subtitle,
             name,
@@ -26,22 +34,14 @@ const gerarCertificado = (title, subtitle, name, initialDescription,
         pdf.create(html, {
             format: 'A4',
             orientation: 'landscape',
-        }).toFile(`certificate ${uuid()}.pdf`, (err, res) => {
+        }).toFile(path, (err, res) => {
             if (err) {
                 reject(err)
             } else {
+                console.log('Disponível em:', res.filename)
                 resolve(res.filename);
             }
         })
     })
 }
 
-(async () => {
-    try {
-       let s = await gerarCertificado('CERTIFICADO', 'Confiro o presente certificado a', 'Niedson Emanoel', 'Por assistir a palestra', 'Growth hacker, uma nova forma de ver o instagram', 'com duração de 1 hora e 30 minutos no evendo the tdcbs news . net', 'Niedson Emanoel', 'Dev Masye')
-      //  .then
-        console.log(s)
-    } catch (e) {
-
-    }
-})();
